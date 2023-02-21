3b6e0139046931ce663c2d3206e18c9664284e7b
Sumon Hossain <106359888+Sumon180@users.noreply.github.com>
2023-02-21 16:20:08 +0600

    Update Store.tsx

--- Dashboard/src/app/Store.tsx
+++ Dashboard/src/app/Store.tsx
@@ -1,35 +1,13 @@
-import {
-  configureStore,
-  combineReducers,
-  ThunkAction,
-  Action,
-} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import toggleSlice from "./toggleSlice";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-const rootReducer = combineReducers({
-  toggle: toggleSlice,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const Store = configureStore({
   reducer: {
-    toggle: persistedReducer,
+    toggle: toggleSlice,
   },
 });
 
 export type RootState = ReturnType<typeof Store.getState>;
+
 export type AppDispatch = typeof Store.dispatch;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+
